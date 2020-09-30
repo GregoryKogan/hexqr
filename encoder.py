b9fc97b806052a98c92556db5e06a1df66ef4955
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-09-30 21:21:42 +0300

    Reed - Solomon codes added to encoder

--- encoder.py
+++ encoder.py
@@ -1,83 +1,59 @@
-def code_to_binary(text):
-    byte_string = bytearray(text, 'utf8')
-    binary_data = ''
-    for byte in byte_string:
-        binary = bin(byte)
-        binary = binary[2::]
-        binary = '0' * (8 - len(binary)) + binary
-        binary_data += binary
-    return binary_data
-
-
-def decode_from_binary(binary_data):
-    byte_list = chunk_string(binary_data, 8)
-    symbols = [chr(int(byte_list[i], 2)) for i in range(len(byte_list))]
-    data = ''.join(symbols)
-    return data
-
-
-def chunk_string(line, length):
-    chunks = [line[0 + i:length + i] for i in range(0, len(line), length)]
-    return chunks
-
-
-def from_binary_to_2bit(data):
-    if len(data) != 2:
-        raise ValueError
-    else:
-        if data == '00':
-            return '0'
-        elif data == '01':
-            return '1'
-        elif data == '10':
-            return '2'
-        elif data == '11':
-            return '3'
-        else:
-            raise ValueError
-
-
-def from_2bit_to_binary(data):
-    if data == '0':
-        return '00'
-    elif data == '1':
-        return '01'
-    elif data == '2':
-        return '10'
-    elif data == '3':
-        return '11'
-    else:
-        raise ValueError
-
-
-def code_to_2bit(binary_data):
-    byte_list = chunk_string(binary_data, 2)
-    pieces_2bit = [from_binary_to_2bit(byte_list[i]) for i in range(len(byte_list))]
-    data = ''.join(pieces_2bit)
-    return data
-
-
-def decode_to_binary(data_2bit):
-    pieces_2bit = [from_2bit_to_binary(data_2bit[i]) for i in range(len(data_2bit))]
-    data = ''.join(pieces_2bit)
-    return data
+from reedsolo import RSCodec, ReedSolomonError
 
 
 def code(data):
-    binary = code_to_binary(data)
-    data_2bit = code_to_2bit(binary)
-    return data_2bit
+    data = bytes(data, encoding='utf8')
+    rsc = RSCodec(10)
+    coded_data = rsc.encode(data)
+    binary_coded_data = ''
+    for byte in coded_data:
+        binary = bin(byte)[2::]
+        binary = '0' * (8 - len(binary)) + binary
+        binary_coded_data += binary
+    quart_coded_data = ''
+    for i in range(0, len(binary_coded_data), 2):
+        byte_pair = binary_coded_data[i] + binary_coded_data[i + 1]
+        if byte_pair == '00':
+            quart_coded_data += '0'
+        elif byte_pair == '01':
+            quart_coded_data += '1'
+        elif byte_pair == '10':
+            quart_coded_data += '2'
+        elif byte_pair == '11':
+            quart_coded_data += '3'
+        else:
+            raise ValueError
+    return quart_coded_data
 
 
-def decode(data_2bit):
-    binary = decode_to_binary(data_2bit)
-    data = decode_from_binary(binary)
-    return data
+def decode(quart_coded_data):
+    binary_coded_data = ''
+    for byte in quart_coded_data:
+        if byte == '0':
+            binary_coded_data += '00'
+        elif byte == '1':
+            binary_coded_data += '01'
+        elif byte == '2':
+            binary_coded_data += '10'
+        elif byte == '3':
+            binary_coded_data += '11'
+        else:
+            raise ValueError
+    coded_data = []
+    for bit_chunk in range(0, len(binary_coded_data), 8):
+        chunk_data = ''
+        for bit_ind in range(8):
+            chunk_data += binary_coded_data[bit_chunk + bit_ind]
+        value = int(chunk_data, 2)
+        coded_data.append(value)
+    coded_data = bytes(coded_data)
+    rsc = RSCodec(10)
+    decoded_data = rsc.decode(coded_data)[0].decode()
+    return decoded_data
 
 
 if __name__ == '__main__':
-    res = code('I Love You!')
-    print(f'Cells: {len(res)}')
-    print(res)
-    res = decode(res)
-    print(res)
+    encoded = code('42')
+    print(encoded)
+    decoded = decode(encoded)
+    print(decoded)
