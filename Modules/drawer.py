c2d7db20a15e95b575a441d6daf5c320ebd0b857
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-10-31 22:34:23 +0300

    rewrote drawer script

--- Modules/drawer.py
+++ Modules/drawer.py
@@ -1,85 +1,172 @@
+import encoder
 from PIL import Image, ImageDraw
 import math
-import encoder
 import random
 
 
-def draw_regular_polygon(img, vertex_num, position, radius, offset_angle=0.0, color=(255, 255, 255, 255)):
-    draw = ImageDraw.Draw(img)
-    pos_x, pos_y = position
+# Returns square black RGBA image of given width
+def get_square_image(width):
+    img = Image.new('RGBA', (width, width), color=(0, 0, 0, 0))
+    return img
+
+
+# Draws hexagon on given image of given radius and color
+def draw_hexagon(img, radius=None, color=(255, 255, 255, 255)):
+    if not radius:
+        radius = min(img.size) / 2
+        
     vertexes = []
-    for i in range(vertex_num):
-        angle = 2 * math.pi / vertex_num * i + offset_angle
-        vertex_x = pos_x + math.cos(angle) * radius
-        vertex_y = pos_y + math.sin(angle) * radius
-        vertex = (vertex_x, vertex_y)
-        vertexes.append(vertex)
+    width, height = img.size
+    x_c = width / 2
+    y_c = height / 2
+    for point_i in range(6):
+        angle = math.pi / 3 * point_i
+        x_i = x_c + math.cos(angle) * radius
+        y_i = y_c - math.sin(angle) * radius
+        vertexes.append((x_i, y_i))
+    draw = ImageDraw.Draw(img)
     draw.polygon(vertexes, fill=color)
     return img
 
 
-def draw_triangle(img, position, orientation, side, color=(255, 255, 255, 255)):
+# Returns row, position and orientation of a triangle with given index in a hexagon with given size
+def position_by_index(index, n):
+    cells_at_current_row = n * 2 + 1
+    current_pos = 1
+    current_row = 1
+    current_index = 1
+    while current_index < index:
+        current_index += 1
+        current_pos += 1
+        if current_pos > cells_at_current_row:
+            current_pos = 1
+            current_row += 1
+            if current_row <= n:
+                cells_at_current_row += 2
+            elif current_row != n + 1:
+                cells_at_current_row -= 2
+
+    if current_row <= n:
+        if current_pos % 2 == 0:
+            orientation = 'D'
+        else:
+            orientation = 'U'
+    else:
+        if current_pos % 2 == 0:
+            orientation = 'U'
+        else:
+            orientation = 'D'
+    return current_row, current_pos, orientation
+
+
+# Returns index of a triangle with given position in a hexagon with given size
+def index_by_position(row, position, n):
+    cells_at_current_row = n * 2 + 1
+    current_pos = 1
+    current_row = 1
+    current_index = 1
+    if current_row == row and current_pos == position:
+        return current_index
+    while current_index < n*n*6:
+        current_index += 1
+        current_pos += 1
+        if current_pos > cells_at_current_row:
+            current_pos = 1
+            current_row += 1
+            if current_row <= n:
+                cells_at_current_row += 2
+            elif current_row != n + 1:
+                cells_at_current_row -= 2
+        if current_row == row and current_pos == position:
+            return current_index
+    return None
+
+
+# Get height of equilateral triangle with given side length
+def get_height_of_equilateral_triangle(side):
+    return 3**0.5 / 2 * side
+
+
+# Get triangle coordinates in image by its position inside hexagon
+def coordinates_by_position(row, position, n, radius, width):
+    triangle_width = radius / n
+    triangle_height = get_height_of_equilateral_triangle(triangle_width)
+    square_width = radius * 2
+
+    vertical_gap = (2 - 3**0.5) / 4 * square_width
+    vertical_gap += (width - square_width) / 2
+    y_coordinate = vertical_gap + triangle_height / 2 + (row - 1) * triangle_height
+
+    if row <= n:
+        row = n - row + 1
+    if row > n:
+        row = row - n
+
+    max_gap = square_width / 4
+    const_gap = (width - square_width) / 2
+    horizontal_gap = (max_gap / (2 * n)) * 2 * (row - 1) + const_gap
+    x_coordinate = horizontal_gap + triangle_width / 2 * position
+
+    return x_coordinate, y_coordinate
+
+
+def draw_equilateral_triangle(image, position, width, orientation, color=(255, 255, 255, 255)):
+    height = 3 ** 0.5 / 2 * width
     pos_x, pos_y = position
-    height = 3 ** 0.5 / 2 * side
     if orientation == 'U':
         vertexes = [
             (pos_x, pos_y - height / 2),
-            (pos_x - side / 2, pos_y + height / 2),
-            (pos_x + side / 2, pos_y + height / 2)
+            (pos_x - width / 2, pos_y + height / 2),
+            (pos_x + width / 2, pos_y + height / 2)
         ]
     else:
         vertexes = [
             (pos_x, pos_y + height / 2),
-            (pos_x - side / 2, pos_y - height / 2),
-            (pos_x + side / 2, pos_y - height / 2)
+            (pos_x - width / 2, pos_y - height / 2),
+            (pos_x + width / 2, pos_y - height / 2)
         ]
-    draw = ImageDraw.Draw(img)
+    draw = ImageDraw.Draw(image)
     draw.polygon(vertexes, fill=color)
-    return img
+    return image
 
 
-def get_body(size=500):
-    body = Image.new('RGBA', (size, size), color=(0, 0, 0, 0))
-    body = draw_regular_polygon(body, 6, (size / 2, size / 2), size / 2, 0)
-    body = draw_regular_polygon(body, 6, (size / 2, size / 2), size / 2 - size / 150, 0, (0, 0, 0, 255))
-    return body
+def count_n_for_data(data_length):
+    final_length = data_length + 18
+    for n in range(1, 25):
+        if n * n * 6 >= final_length:
+            return n
+    return None
 
 
-def get_cell_position_data(cell_index, cells_per_side):
-    total_cells = 6 * cells_per_side ** 2
-    if cell_index > total_cells / 2:
-        cell_index -= total_cells / 2
-        row = cells_per_side
-        cells_last_row = cells_per_side * 2 + 1 + 2 * (cells_per_side - 1)
-        while cell_index > 0:
-            row += 1
-            cell_index -= cells_last_row
-            cells_last_row -= 2
-        position = cell_index + cells_last_row + 2
-        orientation = 'D' if position % 2 != 0 else 'U'
-    else:
-        row = 0
-        cells_last_row = cells_per_side * 2 + 1
-        while cell_index > 0:
-            row += 1
-            cell_index -= cells_last_row
-            cells_last_row += 2
-        position = cell_index + cells_last_row - 2
-        orientation = 'U' if position % 2 != 0 else 'D'
-    return row, position, orientation
+def get_avoid_indexes(n):
+    avoid_positions = [
+        (1, 1),
+        (1, 2),
+        (1, 2 * n),
+        (1, 2 * n + 1),
+        (n, 1),
+        (n + 1, 1),
+        (n, 4 * n - 1),
+        (n + 1, 4 * n - 1),
+        (2 * n, 1),
+        (2 * n, 2),
+        (2 * n, 2 * n),
+        (2 * n, 2 * n + 1),
+        (n, 2 * n - 1),
+        (n, 2 * n),
+        (n, 2 * n + 1),
+        (n + 1, 2 * n - 1),
+        (n + 1, 2 * n),
+        (n + 1, 2 * n + 1)
+    ]
+    avoid_indexes = []
+    for position in avoid_positions:
+        index = index_by_position(position[0], position[1], n)
+        avoid_indexes.append(index)
+    return avoid_indexes
 
 
-def get_row_offset(row, cells_per_side, radius):
-    cell_side = radius / cells_per_side
-    if row > cells_per_side:
-        row -= cells_per_side
-        offset = cell_side * 0.5 * row - cell_side/2
-    else:
-        offset = cell_side * 0.5 * (cells_per_side - row)
-    return offset
-
-
-def write_data(img, data, radius, color_map=None):
+def write_data(image, radius, data, color_map=None):
     if not color_map:
         color_map = {
             0: (255, 255, 255, 255),
@@ -87,28 +174,101 @@ def write_data(img, data, radius, color_map=None):
             2: (188, 14, 76, 255),
             3: (255, 197, 1, 255)
         }
-    width, height = img.size
-    center_x, center_y = width / 2, height / 2
-    cells_per_side = 7
-    cell_side = radius / cells_per_side
-    cell_height = 3**0.5/2 * cell_side
-    total_cells = 6 * cells_per_side ** 2
-    for cell_index in range(1, total_cells + 1):
-        row, position, orientation = get_cell_position_data(cell_index, cells_per_side)
-        offset = get_row_offset(row, cells_per_side, radius)
-        pos_x = center_x - radius + cell_side/2 * position + offset
-        pos_y = center_y - radius * 3**0.5/2 + cell_height/2 + (row - 1) * cell_height
-        img = draw_triangle(img, (pos_x, pos_y), orientation, cell_side, color_map[random.randint(0, 3)])
-    return img
+
+    data = '0123' + data
+    width = min(image.size)
+    n = count_n_for_data(len(data))
+    triangle_width = radius / n
+    
+    avoid_indexes = get_avoid_indexes(n)
+
+    bits_done = 0
+    for index in range(1, n * n * 6 + 1):
+        if index not in avoid_indexes:
+            if bits_done < len(data):
+                bit = int(data[bits_done])
+                bits_done += 1
+            else:
+                bit = random.randint(0, 3)
+            row, position, orientation = position_by_index(index, n)
+            coordinate_x, coordinate_y = coordinates_by_position(row, position, n, radius, width)
+            image = draw_equilateral_triangle(image, (coordinate_x, coordinate_y), triangle_width, orientation,
+                                              color=color_map[bit])
+    return image
 
 
-def build_code(data, size=500, path=None, show=False):
-    body = get_body(size)
-    code = write_data(body, data, size / 2 - size / 75)
-    # code = code.rotate(90)
-    code.show()
+def draw_markers(image, data_length, radius, color_map=None):
+    if not color_map:
+        color_map = {
+            1: (53, 79, 96, 255)
+        }
 
+    n = count_n_for_data(data_length)
+    width = min(image.size)
+    triangle_width = radius / n
+
+    top_corner = [(1, 1), (1, 2)]
+    for point in top_corner:
+        index = index_by_position(point[0], point[1], n)
+        row, position, orientation = position_by_index(index, n)
+        coordinate_x, coordinate_y = coordinates_by_position(row, position, n, radius, width)
+        image = draw_equilateral_triangle(image, (coordinate_x, coordinate_y), triangle_width, orientation,
+                                          color=color_map[1])
+    other_corners = [
+        (1, 2 * n),
+        (1, 2 * n + 1),
+        (n, 1),
+        (n + 1, 1),
+        (n, 4 * n - 1),
+        (n + 1, 4 * n - 1),
+        (2 * n, 1),
+        (2 * n, 2),
+        (2 * n, 2 * n),
+        (2 * n, 2 * n + 1)
+    ]
+    for point in other_corners:
+        index = index_by_position(point[0], point[1], n)
+        row, position, orientation = position_by_index(index, n)
+        coordinate_x, coordinate_y = coordinates_by_position(row, position, n, radius, width)
+        image = draw_equilateral_triangle(image, (coordinate_x, coordinate_y), triangle_width, orientation,
+                                          color=(255, 255, 255, 255))
+
+    height = 3 ** 0.5 / 2 * triangle_width
+    top_point = list(coordinates_by_position(n, 2 * n - 1, n, radius, width))
+    top_point[1] -= height / 2
+    top_point = tuple(top_point)
+    
+    left_point = list(coordinates_by_position(n + 1, 2 * n - 1, n, radius, width))
+    left_point[1] += height / 2
+    left_point = tuple(left_point)
+
+    right_point = list(coordinates_by_position(n, 2 * n + 1, n, radius, width))
+    right_point[0] += triangle_width / 2
+    right_point[1] += height / 2
+    right_point = tuple(right_point)
+
+    center_triangle_vertexes = [top_point, left_point, right_point]
+
+    draw = ImageDraw.Draw(image)
+    draw.polygon(center_triangle_vertexes, fill=(255, 255, 255, 255), outline=(0, 0, 0, 255))
+    return image
+
+
+def build_code(data, size=1000, color_map=None, orientation=None):
+    code_image = get_square_image(size)
+    code_image = draw_hexagon(code_image, radius=min(code_image.size) / 2, color=(255, 255, 255, 255))
+    code_image = draw_hexagon(code_image, radius=min(code_image.size) / 2 * 0.99, color=(0, 0, 0, 255))
+    code_image = draw_markers(code_image, len(data), radius=min(code_image.size) / 2 * 0.97, color_map=color_map)
+    code_image = write_data(code_image, data=data, radius=min(code_image.size) / 2 * 0.97, color_map=color_map)
+    if orientation == 'CRYSTAL':
+        code_image = code_image.resize((size * 5, size * 5))
+        code_image = code_image.rotate(-30)
+        code_image.thumbnail((size, size), Image.ANTIALIAS)
+    return code_image
+    
 
 if __name__ == '__main__':
     data_to_code = encoder.code('https://youtu.be/dQw4w9WgXcQ')
-    build_code(data_to_code, size=1000, path='Resources/Code.png', show=True)
+    code = build_code(data_to_code, size=1000, orientation='CRYSTAL')
+    code.show()
+    code.save('Resources/HEXCode.png')
